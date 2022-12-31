fd89bfc694b5787da2a471046c20c1c82725e13b
New-Bucket <azizjonnormatov437@gmail.com>
2022-12-31 23:28:40 +0500

    file

---
+++ src/hooks/use-photos.js
@@ -0,0 +1,41 @@
+import { useEffect, useState, useContext } from 'react'
+import UserContext from '../context/user'
+import {getPhotos, getUserByUserId } from '../services/firebase'
+
+/**
+ * 
+ * @returns main home page feed component
+ */
+export default function usePhotos(){
+
+    //list of photos
+    const [photos, setPhotos] = useState(null)
+
+    //current user
+    const {
+        user: { uid : userId = '' }
+    } = useContext(UserContext)
+
+    useEffect(() => {
+        async function getTimeLinePhotos(){
+
+            //extract following from user's info
+            const [{ following }] = await getUserByUserId(userId)
+            let followedUserPhotos = []
+
+            //if user has following, get photos
+            if( following.length > 0){
+                followedUserPhotos = await getPhotos(userId, following)
+            }
+
+            //sort photos by date descending
+            followedUserPhotos.sort((a, b) => b.dateCreated - a.dateCreated)
+            setPhotos(followedUserPhotos)
+        }
+
+        getTimeLinePhotos()
+        
+    }, [userId])
+
+return { photos }
+}
